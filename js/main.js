e2b6ea177168c20c8d2e83cd9eb7772e641a6f94
Pietro <pietro.g.souza@aluno.senai.br>
2026-02-26 10:11:43 -0300

    Criando consts e linkando os inputs

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// 1. Lógica do Menu Principal
+const btnP1 = document.querySelector('#inputP1');
+const btnP2 = document.querySelector('#inputP2');
+const btnP3 = document.querySelector('#inputP3');
+console.log(btnP1, btnP2, btnP3);
