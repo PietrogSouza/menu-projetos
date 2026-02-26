1607ef69998ae695a2fa7e324d72a14c2fd8c005
Pietro <pietro.g.souza@aluno.senai.br>
2026-02-26 11:14:47 -0300

    Pegando o envento do botão e usando ele para ir para outra página

--- js/main.js
+++ js/main.js
@@ -2,4 +2,16 @@
 const btnP1 = document.querySelector('#inputP1');
 const btnP2 = document.querySelector('#inputP2');
 const btnP3 = document.querySelector('#inputP3');
-console.log(btnP1, btnP2, btnP3);
+
+// 2. Método addEventListener com uma função anônima
+btnP1.addEventListener('click', function(){
+    window.location.href='./pages/projeto01.html'
+});
+
+btnP2.addEventListener('click', function(){
+    window.location.href='./pages/projeto02.html'
+});
+
+btnP3.addEventListener('click', function(){
+    window.location.href='./pages/projeto03.html'
+});
